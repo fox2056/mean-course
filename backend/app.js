0f4d65484902fa22a40f06cad4b8137dff3460f4
Oleksii Sliepov <sliepov@icloud.com>
2019-04-18 23:19:26 +0200

    Added mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,67 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Post = require("./models/post");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://olek:rBWQcmUQh3aUqyQT@cluster0-jjhi1.mongodb.net/meanapp?retryWrites=true",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("Connected to database.");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+// rBWQcmUQh3aUqyQT
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
-    console.log(post);
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
     res.status(201).json({
-        message: 'Post added sucessfully!'
+      message: "Post added sucessfully!",
+      postId: createdPost._id
     });
+  });
 });
 
 app.get("/api/posts", (req, res, next) => {
-    const posts = [
-        {id: 'dfsdfsdf', title: 'First post', content: 'Content of first post.'},
-        {id: 'dfsddffsdf', title: 'Second post', content: 'Content of second post.'},
-        {id: 'dfsdsdsfsdf', title: 'Third post', content: 'Content of third post.'},
-    ]
+  Post.find().then(documents => {
     res.status(200).json({
-        message: 'Posts fetchet succesfully!',
-        posts: posts
+      message: "Posts fetchet succesfully!",
+      posts: documents
     });
+  });
 });
 
-module.exports = app; 
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({ _id: req.params.id}).then(result => {
+      console.log(result);
+      res.status(200).json({message: 'Post deleted.'});
+    }); 
+});
+
+module.exports = app;
